e5d52de1067d336875f2e6424f010eff0febee4f
david <vok@live.com>
2015-08-13 23:55:54 +0800

    add database2

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'mysite.books',
+	'hellodjango.books',
 )
 
 MIDDLEWARE_CLASSES = (
