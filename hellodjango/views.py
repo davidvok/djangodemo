452e6b71574e54fafc346e4416d06f84227267e0
david <vok@live.com>
2015-08-13 16:16:29 +0800

    add template

--- hellodjango/views.py
+++ hellodjango/views.py
@@ -1,4 +1,7 @@
 from django.http import Http404, HttpResponse
+#from django.template.loader import get_template
+#from django.template import Context
+from django.shortcuts import render_to_response
 import datetime
 
 def hello(request):
@@ -6,8 +9,10 @@ def hello(request):
 
 def current_datetime(request):
 	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" % now
-	return HttpResponse(html)
+	#t = get_template('current_datetime.html')
+	#html = t.render(Context({'current_date': now}))
+	#html = "<html><body>It is now %s.</body></html>" % now
+	return render_to_response('current_datetime.html', {'current_date': now})
 
 def hours_ahead(request, offset):
 	try:
@@ -15,6 +20,7 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	#assert False
-	html = "<html><body>In %d hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse(html)
+	#html = "<html><body>In %d hour(s), it will be %s.</body></html>" % (offset, dt)
+	#return HttpResponse(html)
+	return render_to_response('hours_ahead.html',{'hour_offset': offset,'next_time': dt })
+
